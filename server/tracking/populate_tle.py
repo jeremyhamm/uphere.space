2c64cf115078a3d9a29f4f33163bcec0c5dd70da
Jeremy Hamm <jeremythamm@gmail.com>
2019-11-27 19:21:51 -0800

    Update tle script with prod values

--- server/tracking/populate_tle.py
+++ server/tracking/populate_tle.py
@@ -19,7 +19,7 @@ def read_satellite_file(url):
   return satelliteNames
 
 def connect_redis():
-  pool = pool = redis.ConnectionPool(host='redis', password='RoWxWabXZ%CHSj7No|ZL|\PfvKDS*`Rz', port=6379)
+  pool = pool = redis.ConnectionPool(host='satellite-list-do-user-6401666-0.db.ondigitalocean.com', password='du4riwzdieyv098q', port=25061, connection_class=redis.SSLConnection)
   return redis.Redis(connection_pool=pool)
 
 def save_to_redis(name, elements):
