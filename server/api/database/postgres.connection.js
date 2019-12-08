950c75d9d14b799e35ab5e116732941d92d02350
Jeremy Hamm <jeremythamm@gmail.com>
2019-12-08 09:23:39 -0800

    Complete revert of connection string

--- server/api/database/postgres.connection.js
+++ server/api/database/postgres.connection.js
@@ -1,19 +1,18 @@
 const pgp = require("pg-promise")({
   capSQL: true
 });
-const connectionString = `
-  postgres://
-  ${process.env.POSTGRES_USER}
-  : 
-  ${process.env.POSTGRES_PASSWORD}
-  @
-  ${process.env.POSTGRES_HOST} 
-  : 
-  ${process.env.POSTGRES_PORT}
-  /
-  ${process.env.POSTGRES_DATABASE}
-  ?ssl=true
-`;
-const connection = pgp(connectionString);
+const connection = pgp(
+  "postgres://" + 
+  process.env.POSTGRES_USER + 
+  ":" + 
+  process.env.POSTGRES_PASSWORD + 
+  "@" + 
+  process.env.POSTGRES_HOST + 
+  ":" + 
+  process.env.POSTGRES_PORT + 
+  "/" + 
+  process.env.POSTGRES_DATABASE +
+  "?ssl=true"
+);
 
 module.exports = connection;
