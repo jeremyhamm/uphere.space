f66368c0e2e4813fd1fc798bd1b6257651131742
Jeremy Hamm <jeremythamm@gmail.com>
2019-12-08 09:26:17 -0800

    Update string with es6 syntax

--- server/api/database/postgres.connection.js
+++ server/api/database/postgres.connection.js
@@ -1,18 +1,18 @@
 const pgp = require("pg-promise")({
   capSQL: true
 });
-const connection = pgp(
-  "postgres://" + 
-  process.env.POSTGRES_USER + 
-  ":" + 
-  process.env.POSTGRES_PASSWORD + 
-  "@" + 
-  process.env.POSTGRES_HOST + 
-  ":" + 
-  process.env.POSTGRES_PORT + 
-  "/" + 
-  process.env.POSTGRES_DATABASE +
-  "?ssl=true"
-);
+const connection = pgp(`
+  postgres://
+  ${process.env.POSTGRES_USER}
+  : 
+  ${process.env.POSTGRES_PASSWORD}
+  @
+  ${process.env.POSTGRES_HOST} 
+  : 
+  ${process.env.POSTGRES_PORT}
+  /
+  ${process.env.POSTGRES_DATABASE}
+  ?ssl=true
+`);
 
 module.exports = connection;
