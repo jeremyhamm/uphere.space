abb31e070629cc938b122305848f65b68f3a9db4
Jeremy Hamm <jeremythamm@gmail.com>
2020-01-12 12:05:54 -0800

    commit

--- server/api/database/redis.connection.js
+++ server/api/database/redis.connection.js
@@ -15,12 +15,4 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-
-let client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  tls: true
-});
-
 module.exports = client;
