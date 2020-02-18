e5ec74d485c03162afaff9e3f3b59a2826013775
Jeremy Hamm <jeremythamm@gmail.com>
2020-02-17 17:00:05 -0800

    Testing

--- server/api/controllers/satellite.controller.js
+++ server/api/controllers/satellite.controller.js
@@ -82,7 +82,7 @@ exports.getSatelliteLocation = (req, res) => {
         'speed': satelliteSpeed,
         'visibility': visibility,
         'footprint_radius': satelliteService.getVisibleFootprint(positionGd.height),
-        'temp': req.cookies.settings.units || "N/A"
+        'temp': req.cookies.settings || "N/A"
       };
       
       // Add track if request is from app
