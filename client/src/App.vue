af651bd1ed1e1f74ec66bb381199a63d7ffd7aae
Jeremy Hamm <jh@jeremyhamm.dev>
2020-05-24 21:58:26 -0700

    Update npm modules

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,12 +14,22 @@
 import utilsMixin from "@/mixins/utils.mixin";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+//import ScriptService from "@/services/script.service";
 export default {
   name: "app",
   mixins: [utilsMixin],
   components: {
     "app-header": Header,
     "app-footer": Footer
+  },
+  created() {
+    // Load sharethis.com buttons
+    // const attributes = {
+    //   "type": "text/javascript",
+    //   "src":"https://platform-api.sharethis.com/js/sharethis.js#property=5eb5dcb5cb9da90012156148&product=inline-share-buttons",
+    //   "async": "async"
+    // };
+    // ScriptService.loadScript(attributes);
   }
 };
 </script>
