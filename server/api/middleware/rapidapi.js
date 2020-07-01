bd54c57d5932559b7e5a2554809e760630abeb8c
Jeremy Hamm <jh@jeremyhamm.dev>
2020-06-30 17:10:37 -0700

    revert

--- server/api/middleware/rapidapi.js
+++ server/api/middleware/rapidapi.js
@@ -28,7 +28,7 @@ const ipValidation = (ip) => {
  * @param {*} next 
  */
 const rapidApiValidation = (req, res, next) => {
-  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'development') {
     return next();
   }
 
